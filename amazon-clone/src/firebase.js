c7f4a9f90f6c89350607b66cd4cb3d1e209c6139
hocastil <Hocastil@ucsc.edu>
2020-09-08 12:01:45 -0700

    firebase

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBve6DMiQlnNeiqmNDEqDtQJEFIjzh0WbU",
+    authDomain: "clone-b67b9.firebaseapp.com",
+    databaseURL: "https://clone-b67b9.firebaseio.com",
+    projectId: "clone-b67b9",
+    storageBucket: "clone-b67b9.appspot.com",
+    messagingSenderId: "890988206096",
+    appId: "1:890988206096:web:9d058a8cb4166bb60b5295",
+    measurementId: "G-E8LW9TX2HP"
+  };
