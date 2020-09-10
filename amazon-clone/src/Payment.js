bb2c7eb1b6dad3e4f7314104afbf26906a3587b4
hocastil <Hocastil@ucsc.edu>
2020-09-10 16:55:30 -0700

    still adding stripe

--- amazon-clone/src/Payment.js
+++ amazon-clone/src/Payment.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Payment.css'
 import { useStateValue } from "./StateProvider";
-import CheckoutProduct from './CheckoutProduct';
+import  CheckoutProduct  from './CheckoutProduct';
 import { Link } from "react-router-dom";
-import { useElements, useStripe, CardElements } from '@stripe/react-stripe-js';
+import { useElements, useStripe, CardElement } from '@stripe/react-stripe-js';
+import  CurrencyFormat  from "react-currency-format";
+import { getBasketTotal } from "./reducer";
 
 function Payment() {
 
@@ -11,6 +13,38 @@ function Payment() {
 
     const stripe = useStripe();
     const elements = useElements();
+
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("")
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+    const [clientSecret, setClientSecret] = useState(true);
+
+    useEffect(() => {
+        // generate the special stripe secret which allows us to charge a customer
+        
+        const getClientSecret = async () => {
+            const response = await axios
+        }
+
+        getClientSecret();
+        }, [basket])
+
+    const handleSubmit = async (event) => {
+        // do all the fancy stripe stuff
+        event.preventDefault(); // stops refreshing
+        setProcessing(true); // keep them from pressing the "buy now" button many times
+
+
+        // const payload = await stripe
+    }
+
+    const handleChange = event => {
+        // Listen for changes in the CardElement
+        // and display any errors as the customer types their card details
+        setDisabled(event.empty);
+        setError(event.error ? event.error.message : "")
+    }
 
     return (
         <div className= "payment" >
@@ -30,7 +64,7 @@ function Payment() {
                     </div>
                     <div className = 'payment_address'>
                         <p>{user?.email}</p>
-                        <p>123 React</p>
+                        <p>123 React Lane</p>
                         <p>Los Angeles, CA</p>
                     </div>
                 </div>
@@ -55,7 +89,30 @@ function Payment() {
                         <h3>Payment Method</h3>
                     </div>        
                     <div className = 'payment__details'>
-                        
+                        <form onSubmit = {handleSubmit}>
+                            <CardElement 
+                                onChange={handleChange}
+                            />
+                            <div className = "payment__priceContainer">
+                                <CurrencyFormat
+                                    renderText={(value) => (
+                                    <h3>Order Total: {value}</h3>
+                                    )}
+                                    decimalScale={2}
+                                    value={getBasketTotal(basket)}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    prefix={"$"}
+                                />
+                                <button disabled = {processing || disabled || succeeded}>
+                                    <span>
+                                        {processing ? <p>Processing</p> : "Buy Now"}
+                                    </span>
+                                </button>
+                            </div>
+                            {/* Errors */}
+                            {error && <div>{error}</div>}
+                        </form>
                     </div>       
                 </div>
             </div>
